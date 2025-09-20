066dc78727f1aa16f77e1b59556da957b37839bd
syed mohsin <mohsinabdul729@gmail.com>
2025-09-20 11:36:19 +0000

    Try fixing this error: `Runtime Error: Error: CardFooter is not defined.

--- src/app/(main)/job-prep/page.tsx
+++ src/app/(main)/job-prep/page.tsx
@@ -6,6 +6,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
